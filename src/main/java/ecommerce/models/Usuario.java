7e644851e294894e5f0e79bda8565d25329f2ede
Adrián Gabás <adriangabasandreu@gmail.com>
2026-02-09 18:59:38 +0100

    Clase models/Usuario

--- src/main/java/ecommerce/models/Usuario.java
+++ src/main/java/ecommerce/models/Usuario.java
@@ -1,6 +1,7 @@
 package ecommerce.models;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class Usuario {
     private int id;
@@ -10,25 +11,78 @@ public class Usuario {
     private String password;
     private String rol;
     private String telefono;
-    private LocalDate fecharegistro;
+    private LocalDateTime fecharegistro;
     private boolean activo;
 
     public Usuario() {
 
     }
 
-    public Usuario(int id, String nombre, String apellido, String email, String password, String rol, String telefono, String fecharegistro, boolean activo) {
+    public Usuario(int id, String nombre, String apellido, String email, String password, String rol, String telefono, LocalDateTime fecharegistro, boolean activo) {
         this.id = id;
         this.nombre = nombre;
         this.apellido = apellido;
         this.email = email;
         this.password = password;
         this.rol = rol;
-        this.telefono = fecharegistro;
+        this.telefono = telefono;
+        this.fecharegistro = fecharegistro;
         this.activo = activo;
     }
 
-
-
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getApellido() {
+        return apellido;
+    }
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getRol() {
+        return rol;
+    }
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+    public String getTelefono() {
+        return telefono;
+    }
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+    public LocalDateTime getFecharegistro() {
+        return fecharegistro;
+    }
+    public void setFecharegistro(LocalDateTime fecharegistro) {
+        this.fecharegistro = fecharegistro;
+    }
+    public boolean isActivo() {
+        return activo;
+    }
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
 
 }
